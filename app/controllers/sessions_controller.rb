a6d17b68d3fabfe27b3a874029b3826571ce67bd
Hoang Ha <SmithMerch2301@gmail.com>
2023-12-24 01:42:24 +0700

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,18 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      # Log the user and redirect to the user's show page
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      log_in @user
-      redirect_to forwarding_url || @user
+      if @user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        log_in @user
+        redirect_to forwarding_url || @user
+      else
+        message = "Account not activated"
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # Create an error message
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,31 +1,65 @@
 require "test_helper"
 
-class UsersIndexTest < ActionDispatch::IntegrationTest
+class UsersIndex < ActionDispatch::IntegrationTest
+
   def setup
-    @admin = users(:michael)
+    @admin     = users(:michael)
     @non_admin = users(:archer)
   end
+end
 
-  test "index as admin including pagination and delete links" do
+class UsersIndexAdmin < UsersIndex
+
+  def setup
+    super
     log_in_as(@admin)
     get users_path
+  end
+end
+
+class UsersIndexAdminTest < UsersIndexAdmin
+
+  test "should render the index page" do
     assert_template 'users/index'
-    assert_select 'div.pagination'  
-    first_page_of_users = User.paginate(page: 1)
+  end
+
+  test "should paginate users" do
+    assert_select 'div.pagination'
+  end
+
+  test "should have delete links" do
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
+  end
+   
+  test "should be able to delete non-admin user" do
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
-      assert_response :see_other
-      assert_redirected_to users_url
     end
+    assert_response :see_other
+    assert_redirected_to users_url
   end
 
-  test "index as non-admin" do
+  test "should display only activated users" do
+    # Deactivate the first user on the page.
+    # Making an inactive fixture user isn't sufficient because Rails can't
+    # guarantee it would appear on the first page.
+    User.paginate(page: 1).first.toggle!('activated')
+    # Ensure that all the displayed users are activated.
+    assigns(:users).each do |user|
+      assert user.activated
+    end
+  end
+end
+
+class UsersNonAdminIndexTest < UsersIndex
+
+  test "should not have delete links as non-admin" do
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
